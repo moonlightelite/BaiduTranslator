53b29e2ba2dbdb503c528f1c7c487aea83ee5d10
tlg <thelastguardian@hotmail.com>
2022-03-12 11:27:41 -0800

    -Added multi-proxies mode -Added --baiducookie due to Baidu cookie invalidation -Code cleanup

--- translate.py
+++ translate.py
@@ -23,11 +23,22 @@ import sys,os
 import re
 import requests
 import argparse
+import pickle
 import time
 import urllib.parse
 import sqlite3
+import random
+
+#python -m pip install requests[socks]
 
 HAVE_PYWIN32 = True
+proxy_mode = False
+# 21CC57D3BED68561D5B35D36D4DB320:FG=1
+# 2BAFEECEB9E308C84DEFCF6B2DB9EB4D:FG=1
+baidu_cookie = "" #
+
+proxies_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'translation-baidu.proxies')
+cookies_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'translation-baidu.cookie')
 try:
     import win32pipe, win32file, pywintypes
 except:
@@ -37,15 +48,13 @@ except:
 class BaiduDecoder():
     i = None
     gtk = None
+    
     def __init__(self, gtk):
         self.gtk = gtk
-
+        
     def zero_fill_right_shift(self, val, n):
         return (val >> n) if val >= 0 else ((val + 0x100000000) >> n)
-
-    def a(self, r):
-        return r
-
+        
     def n(self, r, o):
         for t in range(0, len(o) - 2, 3):
             a = o[t + 2]
@@ -64,8 +73,7 @@ class BaiduDecoder():
         if r > 0x7fffffff:
             r = r - 0xffffffff - 1
         return r
-
-
+        
     def e(self, r):
         o = re.match("[\uD800-\uDBFF][\uDC00-\uDFFF]", r)
         if not o:
@@ -79,7 +87,7 @@ class BaiduDecoder():
             for C in range(h):
                 try:
                     if e[C]:
-                        f = f + a(e[C].split(""))
+                        f = f + e[C].split("")
                 except:
                     pass
                 if C != h - 1:
@@ -88,7 +96,6 @@ class BaiduDecoder():
             if g > 30:
                 r = "".join(x for x in f[0:10]) + "".join(x for x in f[Math.floor(g / 2) - 5:Math.floor(g / 2) + 5]) + \
                     "".join(x for x in f[-10:])
-
         u = 0
         if self.i:
             u = self.i
@@ -97,7 +104,6 @@ class BaiduDecoder():
                 u = self.gtk
             else:
                 u = ""
-
         d = u.split(".")
         m = s = c = 0
         try:
@@ -109,7 +115,6 @@ class BaiduDecoder():
         except:
             s = 0
         S = {}
-
         for v in range(len(r)):
             A = ord(r[v])
             if 128 > A:
@@ -134,25 +139,24 @@ class BaiduDecoder():
                         c+=1
                     S[c] = 63 & A | 128
                     c+=1
-        
         p = m
         F = "+-a^+6"
         D = "+-3^+b+-f"
         for b in range(len(S)):
             p += S[b]
             p = self.n(p, F)
-
         p = self.n(p, D)
         p ^= s
         if 0 > p:
             p = (2147483647 & p) + 2147483648
         p %= 1e6
-
         return str(int(p)) + "." + str(int(p) ^ m)
+
 
 class TranslationException(Exception):
     def __init__(self, message):
         super().__init__(message)
+
 
 class BaiduTranslate():
     url_root = "https://fanyi.baidu.com/"
@@ -161,7 +165,7 @@ class BaiduTranslate():
     headers = {
         "origin": "https://fanyi.baidu.com",
         "referer": "https://fanyi.baidu.com",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
     }
     cookies = {}
     cache = {}
@@ -174,18 +178,43 @@ class BaiduTranslate():
         self.session = requests.session()
         self.session.headers = self.headers
 
+        self.set_proxies()
+        self.set_cookies()
         self.token, self.gtk = self.get_gtk_token()
         self.setupSQLite()
-
+        
+    def set_proxies(self):
+        if proxy_mode and os.path.exists(proxies_file):
+            with open(proxies_file, 'r') as fd:
+                self.proxies = [x.strip() for x in fd.readlines()]
+        else:
+            self.proxies = None
+        
+    def pick_proxy(self):
+        p = random.choice(self.proxies)
+        proxies = {
+            'http': p,
+            'https': p
+        }
+        return proxies
+        
+    def set_cookies(self):
+        if not os.path.exists(cookies_file):
+            page = self.session.get(self.url_root)
+            self.cookies = page.cookies
+        else:
+            with open(cookies_file, 'rb') as fd:
+                self.cookies = pickle.load(fd)
+        if baidu_cookie:
+            self.cookies.set('BAIDUID', baidu_cookie, domain=".baidu.com", path="/")
+            with open(cookies_file, 'wb') as fd:
+                pickle.dump(self.cookies, fd)
 
     def get_gtk_token(self):
-        page = self.session.get(self.url_root)
-        self.cookies = page.cookies
-
         response = self.session.get(self.url_root, cookies=self.cookies)
         response = response.content.decode()
     
-        token = re.findall(r"token: '(.+)'", response)[0]
+        token = re.findall(r"\stoken: '(.+)'", response)[0]
         gtk = re.findall(r";window.gtk = '(.+)';", response)[0]
 
         return token, gtk
@@ -246,18 +275,24 @@ class BaiduTranslate():
 
         decoder = BaiduDecoder(self.gtk)
         sign = decoder.e(input_string)
-            
+
         try:
             data = {
                 "from": input_lang,
                 "to": output_lang,
                 "query": input_string, # urllib.parse.quote_plus(input_string)
-                "transtype": "realtime",
+                "domain": "common",
                 "simple_means_flag": 3,
                 "sign": sign,  
-                "token": self.token
+                "token": self.token,
             }
-            response = self.session.post(self.url_v2transapi, data=data)
+            
+            if proxy_mode:
+                proxy = self.pick_proxy()
+            else:
+                proxy = None
+            response = self.session.post(self.url_v2transapi + "?from=" + input_lang + "&to=" + output_lang, data=data, cookies=self.cookies, proxies=proxy)
+            
             response_dict = response.json()
 
             if "error" in response_dict.keys() and not retry:
@@ -272,7 +307,7 @@ class BaiduTranslate():
                 self.conn.commit()
             return trans
         except TranslationException as e:
-            print("Translation error. Resetting keys")
+            #print("Translation error. Resetting keys")
             self.token, self.gtk = self.get_gtk_token()
             return self._translate(input_string, input_lang, output_lang, retry=True)
         except Exception as e:
@@ -349,10 +384,17 @@ def test(baidu):
     """一天睡3小時、還能4天不休息，周揚青質疑小豬為了約人不睡覺，
     過往受訪片段出爐，羅志祥曾自曝體力好、健檢報告精采，讓網友瞬間理解「時間管理」能力。"""]
 
-    print(samples[0], "\n", baidu.translate(samples[0]))
-    print(samples[1], "\n", baidu.translate(samples[1], output_lang="jp"))
+    print(samples[0], "\n", baidu.translate(samples[0]), "\n\n")
+    print(samples[1], "\n", baidu.translate(samples[1], output_lang="jp"), "\n\n")
 
 def run(args):
+    if args.proxy:
+        global proxy_mode
+        proxy_mode = True
+    if args.baiducookie:
+        global baidu_cookie
+        baidu_cookie = args.baiducookie
+        
     baidu = BaiduTranslate()
     if args.test:
         test(baidu)
@@ -366,13 +408,32 @@ def run(args):
     elif args.input:
         print(baidu.translate(args.input, output_lang=args.output_language))
         sys.exit(0)
+    elif args.filename:
+        for line in args.filename:
+            l = line.strip()
+            if not l:
+                continue
+            print(l, "\n", baidu.translate(l, output_lang=args.output_language).strip(), "\n")
+            time.sleep(1)
+        sys.exit(0)
+        
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return open(arg, 'r')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Translation using Baidu Translate.')
     parser.add_argument('-i', '--input', help='Phrase to be translated', type=str)
-    parser.add_argument('-o', '--output_language', help='Output language (eg. en for English, jp for Japanese, zh for Chinese etc.)', default="en")
+    parser.add_argument('-o', '--output_language', help="Output language (eg. en for English, 'zh': '中文','jp': '日语','yue': '粤语','wyw': '文言文','cht': '中文繁体' etc.)", default="en")
     parser.add_argument('-t', '--test', help='Test mode', default=False, action='store_true')
+    parser.add_argument('-p', '--proxy', help='Use proxy file', default=False, action='store_true')
     parser.add_argument('-d', '--daemon', help='Run as daemon', default=False, action='store_true')
+    parser.add_argument('-bc', '--baiducookie', help='BAIDUID cookie (xxx...:FG=1)', type=str)
+    parser.add_argument("-f", dest="filename", required=False,
+                    help="File to be translated", metavar="FILE",
+                    type=lambda x: is_valid_file(parser, x))
     args = parser.parse_args()
 
     run(args)
